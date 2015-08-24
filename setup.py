0c963ec1268eef9c4c59bbb2c8005a387565bacd
Karl Eklund <keklund@ad.unc.edu>
2015-08-24 12:57:49 -0400

    fixed setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
-      version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
-      url='http://www.python.org/sigs/distutils-sig/',
+setup(name='HTSFTransport',
+      version='0.1',
+      description='High Throughput Seqcuencing Facility Transport',
+      author='Karl Eklund',
+      author_email='keklund@ad.unc.edu',
+      url='http://transport-keklund.apps.unc.edu/',
 #      install_requires=['Django>=1.3'],
      )
